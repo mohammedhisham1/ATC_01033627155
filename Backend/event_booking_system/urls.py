2b0f72f1139fde5e4d88fb8c6c8f33f986a1e2b5
mohammedhisham1 <mohamad92moh@gmail.com>
2025-05-15 18:39:51 +0300

    urls

--- Backend/event_booking_system/urls.py
+++ Backend/event_booking_system/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
+from users.views import RegisterView, UserProfileView
+from events.views import EventViewSet, CategoryViewSet
+from bookings.views import BookingViewSet
+
+# Set up the DRF routers
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'bookings', BookingViewSet, basename='bookings')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/profile/', UserProfileView.as_view(), name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
